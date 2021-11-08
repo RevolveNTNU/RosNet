4385de0babca038838886fb96762b781d8cc5ab9
Inge Grelland <inge@crazy4life.no>
2021-11-08 15:21:16 +0100

    Finished Connection class constructor

--- ROSNET/DataModel/Connection.cs
+++ ROSNET/DataModel/Connection.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
+using ROSNET;
 
 namespace ROSbag_ReadWrite.DataModel
 {
@@ -8,17 +10,54 @@ namespace ROSbag_ReadWrite.DataModel
     {
         public int Conn { get; }
         public string Topic { get; }
-        public string OriginalTopic { get; }
-        public string Type { get; }
-        public string Md5sum { get; }
-        public string MessageDefinition { get; }
-        public string CallerID { get; }
-        public string Latching { get; }
+        public string OriginalTopic { get; private set; }
+        public string Type { get; private set; }
+        public string Md5sum { get; private set; }
+        public string MessageDefinition { get; private set;  }
+        public string CallerID { get; private set; }
+        public string Latching { get; private set; }
 
-        public Connection(int conn, string topic, byte[] data)
+        public Connection(int conn, string topic, BinaryReader reader)
         {
             Conn = conn;
             Topic = topic;
+            SetData(reader);
+        }
+
+        private void SetData(BinaryReader reader)
+        {
+            long endPos = reader.BaseStream.Position + reader.ReadInt32();
+            int fieldLen;
+            string fieldName;
+            string fieldValue;
+            while (reader.BaseStream.Position != endPos)
+            {
+                fieldLen = reader.ReadInt32();
+                fieldName = Header.ReadName(reader, fieldLen);
+                fieldValue = System.Text.Encoding.Default.GetString(reader.ReadBytes(fieldLen - fieldName.Length - 1));
+
+                switch (fieldName)
+                {
+                    case "topic":
+                        OriginalTopic = fieldValue;
+                        break;
+                    case "type":
+                        Type = fieldValue;
+                        break;
+                    case "md5sum":
+                        Md5sum = fieldValue;
+                        break;
+                    case "message_definition":
+                        MessageDefinition = fieldValue;
+                        break;
+                    case "callerid":
+                        CallerID = fieldValue;
+                        break;
+                    case "latching":
+                        Latching = fieldValue;
+                        break;
+                }
+            }
         }
     }
 }
