1dd82c0fdcefcb766a30a4d83edbb5f105da1620
Inge Grelland <inge@crazy4life.no>
2021-11-08 14:59:02 +0100

    Added simple class for Connections

---
+++ ROSNET/DataModel/Connection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ROSbag_ReadWrite.DataModel
+{
+    public class Connection
+    {
+        public int Conn { get; }
+        public string Topic { get; }
+        public string OriginalTopic { get; }
+        public string Type { get; }
+        public string Md5sum { get; }
+        public string MessageDefinition { get; }
+        public string CallerID { get; }
+        public string Latching { get; }
+
+        public Connection(int conn, string topic, byte[] data)
+        {
+            Conn = conn;
+            Topic = topic;
+        }
+    }
+}
