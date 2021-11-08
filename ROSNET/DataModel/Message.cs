78d236a1fb4dd9c553cafa83af794ddc1a5ce710
Inge Grelland <inge@crazy4life.no>
2021-11-08 14:52:32 +0100

    Created simple class for "Message data" records

---
+++ ROSNET/DataModel/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ROSNET.DataModel
+{
+    public class Message
+    {
+        public int Conn { get; }
+        public long Time { get;  }
+        private byte[] Data { get; }
+
+        public Message(int conn, long time, byte[] data)
+        {
+            Conn = conn;
+            Time = time;
+            Data = data;
+        }
+    }
+}
