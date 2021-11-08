76d089eed6497f83a88d004bd9d6eb84eb5828bb
Inge Grelland <inge@crazy4life.no>
2021-11-08 14:45:16 +0100

    Added overload to ROSbag class for initiating with a file
    Binary Reader from file will be used in everything related to creating a ROSbag from a .bag file

--- ROSNET/DataModel/ROSbag.cs
+++ ROSNET/DataModel/ROSbag.cs
@@ -1,11 +1,20 @@
 ﻿using System;
+using System.IO;
+
 namespace ROSNET.DataModel
 {
     public class ROSbag
     {
+        private BinaryReader Reader { get; set; }
+
         public ROSbag()
         {
 
+        }
+
+        public ROSbag(string path)
+        {
+            Reader = new BinaryReader(File.Open(path, FileMode.Open));
         }
     }
 }
