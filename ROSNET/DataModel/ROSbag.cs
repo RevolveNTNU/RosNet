e4732991bbc9bc89964b1cd14bef2cb3ce3a3cdc
Juni-hub <64313114+Juni-hub@users.noreply.github.com>
2021-11-15 16:38:38 +0100

    16 implement ci (#50)
    * Add ability to choose file path, instead of being hard coded (#32)
    
    Getting directory path from showCase insted of Reader Class
    
    * Adding DataModels and renaming namespace to ROSNET
    
    * Adding byte Op in IRecord
    
    * Added overload to ROSbag class for initiating with a file
    
    Binary Reader from file will be used in everything related to creating a ROSbag from a .bag file
    
    * Created simple class for "Message data" records
    
    * Added simple class for Connections
    
    * Finished Connection class constructor
    
    * Created AddConnection in ROSbag class
    
    Also added some summaries to parts of the DataModel code
    
    * Small changes in names of fields
    
    * Create workflows
    
    * Delete workflows
    
    * Create build_and_test.yml
    
    * Create pack_and_release.yml
    
    * Delete build_and_test.yml
    
    * Create build_and_test.yml
    
    * Create learn_github_actions.yml
    
    * Delete learn_github_actions.yml
    
    * Update build_and_test.yml
    
    Co-authored-by: gridmike <54400156+mikaelste@users.noreply.github.com>
    Co-authored-by: mikkemann1 <mikaelsteenbuch@gmail.com>
    Co-authored-by: Inge Grelland <inge@crazy4life.no>

---
+++ ROSNET/DataModel/ROSbag.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ROSNET.DataModel
+{
+    public class ROSbag
+    {
+        private BinaryReader Reader { get; set; }
+        public Dictionary<int, Connection> Connections { get; private set; }
+
+        public ROSbag()
+        {
+
+        }
+
+        public ROSbag(string path)
+        {
+            Reader = new BinaryReader(File.Open(path, FileMode.Open));
+        }
+
+        /// <summary>
+        /// Adds a Connection object to the ROSbag's list of connections
+        /// </summary>
+        /// <returns>true if connection was successfully added</returns>
+        public bool AddConnection(Connection conn)
+        {
+            if (!Connections.ContainsKey(conn.ConnID))
+            {
+                Connections.Add(conn.ConnID, conn);
+                return true;
+            }
+            return false;
+        }
+    }
+}
