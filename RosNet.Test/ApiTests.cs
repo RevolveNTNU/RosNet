a54c7cd538371e2e41301c3ebfc46328a7fc98a8
Juni <junitrigger@gmail.com>
2022-02-18 16:01:55 +0100

    Add new test bag

--- RosNet.Test/ApiTests.cs
+++ RosNet.Test/ApiTests.cs
@@ -13,24 +13,24 @@ namespace RosNet.Test
         [Fact]
         public void TestGetConnectionFields()
         {
-            string path = Directory.GetCurrentDirectory() + "/TestBags/acc19-11.bag";
+            string path = Directory.GetCurrentDirectory() + "/TestBags/2021-05-26-23-16-27.bag";
             RosBag rosBag = RosBagReader.Read(path);
             Dictionary<string, List<string>> fields = rosBag.GetConnectionFields();
 
-            Assert.Equal(39, fields.Count);
-            Assert.True(fields.ContainsKey("/amk/rpm"));
-            Assert.Equal(6, fields["/sbs/steering_wheel"].Count);
+            Assert.Equal(7, fields.Count);
+            Assert.True(fields.ContainsKey("/vectornav/IMU"));
+            Assert.Equal(9, fields["/sbs/steering_wheel"].Count);
 
         }
 
         [Fact]
         public void TestGetTimeSeries()
         {
-            string path = Directory.GetCurrentDirectory() + "/TestBags/acc19-11.bag";
+            string path = Directory.GetCurrentDirectory() + "/TestBags/2021-05-26-23-16-27.bag";
             RosBag rosBag = RosBagReader.Read(path);
             Dictionary<Time, FieldValue> timeSeries = rosBag.GetTimeSeries("/amk/rpm", "FL_vel");
 
-            Assert.Equal(7071, BitConverter.ToInt32(timeSeries[new Time(1629366496, 951464957)].Value));
+            Assert.Equal(-0.001396, BitConverter.ToInt32(timeSeries[new Time(1622063787, 689419290)].Value));
         }
     }
 }
