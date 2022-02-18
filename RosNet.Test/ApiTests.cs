748e3797d515c51ec59e007f5590374d25c162dc
Inge Grelland <inge@crazy4life.no>
2022-02-18 12:50:36 +0100

    Fixed tests

--- RosNet.Test/ApiTests.cs
+++ RosNet.Test/ApiTests.cs
@@ -17,9 +17,9 @@ namespace RosNet.Test
             RosBag rosBag = RosBagReader.Read(path);
             Dictionary<string, List<string>> fields = rosBag.GetConnectionFields();
 
-            Assert.Equal(fields.Count, 39);
+            Assert.Equal(39, fields.Count);
             Assert.True(fields.ContainsKey("/amk/rpm"));
-            Assert.Equal(fields["/sbs/steering_wheel"].Count, 6);
+            Assert.Equal(6, fields["/sbs/steering_wheel"].Count);
 
         }
 
@@ -30,8 +30,7 @@ namespace RosNet.Test
             RosBag rosBag = RosBagReader.Read(path);
             Dictionary<(uint, uint), FieldValue> timeSeries = rosBag.GetTimeSeries("/amk/rpm", "FL_vel");
 
-            Assert.True(BitConverter.ToInt32(timeSeries[(1629366496, 951464957)].Value), 7071);
-
+            Assert.Equal(7071, BitConverter.ToInt32(timeSeries[(1629366496, 951464957)].Value));
         }
     }
 }
