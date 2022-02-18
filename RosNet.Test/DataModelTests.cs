c5c4fb25a13db494590200bad57d9e553a71df97
Inge Grelland <48064053+Kytzis@users.noreply.github.com>
2022-02-18 13:03:44 +0100

    Added Time class (#70)
    * Added Time class
    
    * Added GetDateTime
    
    * Changed to more accurate toString format
    
    * Added test of ToDateTime

---
+++ RosNet.Test/DataModelTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+using RosNet.DataModel;
+
+namespace RosNet.Test
+{
+    public class DataModelTests
+    {
+        [Fact]
+        public void TestToDateTime()
+        {
+            // Epoch time corresponding to
+            // 2022-02-15 14:35:08.1234567
+            uint epochSec = 1644935708;
+            uint epochNano = 123456700;
+            Time test = new Time(epochSec, epochNano);
+            // 1 tick is 100 nano seconds
+            Assert.Equal(test.ToDateTime(), new DateTime(2022, 02, 15, 14, 35, 8).AddSeconds(epochNano * Math.Pow(10, -9)));
+        }
+    }
+}
